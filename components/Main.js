b6db6423ba1d946a7feac8422633a32ffc884d16
ash_rick <ashish.gupta@aurigait.com>
2022-05-09 01:26:09 +0530

    calculate total balance and sanity query

--- components/Main.js
+++ components/Main.js
@@ -1,23 +1,33 @@
-import React from 'react'
-import styled from 'styled-components'
-import Portfolio from './Portfolio'
+import React from "react";
+import styled from "styled-components";
+import Portfolio from "./Portfolio";
+import Promos from "./Promos";
 
-const Main = () => {
+const Main = ({ twTokens, sanityTokens, walletAddress }) => {
   return (
     <Wrapper>
-        <Portfolio />
+      <Portfolio
+        twTokens={twTokens}
+        sanityTokens={sanityTokens}
+        walletAddress={walletAddress}
+      />
+      <Promos />
     </Wrapper>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
 
 const Wrapper = styled.div`
   display: flex;
   max-height: calc(100vh - 64px);
-  overflow: scroll;
+  overflow: hidden;
+  overflow-y: scroll;
+  ::webkit-scrollbar {
+    display: none;
+  }
 
   & div {
-      border-radius: 0.4rem;
+    border-radius: 0.4rem;
   }
-`
+`;
